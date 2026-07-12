9ef962a44c5804a419d7ab656d6621af9ca9cbd1
dodos <dodos@Dodos-Mac.local>
2026-07-12 01:41:26 -0400

    Add checkbox to folders in Edit mode

--- CopyM8/Views/Components/ClipboardFolderView.swift
+++ CopyM8/Views/Components/ClipboardFolderView.swift
@@ -31,7 +31,9 @@ struct ClipboardFolderView: View {
         Button(action: onTap) {
             HStack(spacing: 12) {
                 if isEditMode {
-                    Spacer().frame(width: 14)
+                    Image(systemName: isChecked ? "checkmark.circle.fill" : "circle")
+                        .foregroundColor(isChecked ? .primary : .primary.opacity(0.3))
+                        .font(.system(size: 14))
                 } else {
                     if let sIndex = shortcutIndex, sIndex < 26 {
                         let shortcutLetter = String(UnicodeScalar(UInt8(65 + sIndex)))
